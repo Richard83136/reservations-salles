e9fb0aee0d9aff6d4800f8393e1f6dbd1f230d64
richard-schulze <richard.schulze@laplateforme.io>
2023-02-09 12:41:35 +0100

    commit mise a jour connexion.php et inscription.php

---
+++ connexion.php
@@ -0,0 +1,128 @@
+<?php 
+
+
+
+include_once("dbconnect.php");
+        
+        // $login = ($_POST['login']);
+        // $password = ($_POST['password']);   
+if ( isset($_POST['submit']))
+{
+    
+    //vérification que l'utilisateur existe bien dans la bdd
+    $requete = $bdd->prepare(' SELECT * FROM utilisateurs where login = :login');
+    $requete->execute(['login' => $_POST['login']]);
+    $result = $requete->fetch();
+    if ( $result == true)
+    {
+            if  ( $_POST['password'] AND $_POST['login']) //vérification si la connection concerne le compte admin
+                { session_start();// ouverture de la session admin
+                    $req = $bdd->prepare('SELECT * FROM utilisateurs  WHERE login  = :login');
+                    $req->execute(array('login' => $_POST['login']));
+                    $_SESSION = $req->fetch();
+                    $_SESSION['login'] = $_POST['login'];
+                    header('Location: index.php');//redirection
+                }
+            else 
+                {
+                        if ( password_verify($_POST['password'],$result['password']))// sinon certification du mpd, pour ouvrir une session utilisateur classique
+                            {
+                                session_start();
+                                $req = $bdd->prepare('SELECT * FROM utilisateurs  WHERE login  = :login');
+                                $req->execute(array('login' => $_POST['login']));
+                                $_SESSION = $req->fetch();
+                                $_SESSION['login'] = $_POST['login'];
+                                
+                                header('Location: connexion.php');//redirection
+                            }
+                        else 
+                        {
+                            ?> <p class='alert alert-danger alert-dismissible fade show'> identifiants incorrects </p>;
+	<?php
+                            
+                        }
+                }
+    }
+    else
+    {
+		?>
+        <p class='alert alert-danger alert-dismissible fade show'> identifiants incorrects </p> 
+	<?php
+    } 
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Page Connexion</title>
+</head>
+
+    <header>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Reservation de salles</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Retour</a>
+              </li>  
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="inscription.php">Inscription</a>
+              </li>
+              
+              
+              
+             </ul>
+            </div>
+        </div>
+      </nav>
+</header>
+<body>
+<div class="parallax_connexion">  
+    <h1 class="text-center pt-5">Bienvenue sur la page de connexion </h1>
+    <div class="container  " id="page_centrale_connexion">
+<div class="row h-100  ">
+    <div class="col-12 h-100 d-flex justify-content-center align-items-center">
+            <form class="w-50"  action="connexion.php" method="post">
+                        <p class="text-center"> <?php  echo @$mauvaisidentifiants;  ?> </p>
+                        <div class="form-group">
+                            <label for="login"><b>Login</b></label>
+                            <input  type="login" name="login" required class="form-control" style="border:1px solid black;" id="login" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1"><b>Password</b></label>
+                            <input type="password" name="password" required class="form-control" style="border:1px solid black;" id="exampleInputPassword1">
+                        </div>
+                        <div class="row">
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                        <button type="submit" name="submit" class="btn btn-info mt-3 ">Connexion</button></div>
+							<br>
+                        <div class="ins">
+							
+                            <p class="alert alert-info alert-dismissible fade show mt-3 rounded" style="border:1px solid black;">Vous n'êtes pas encore inscrit ?</p>
+                            <div class="d-grid gap-2 col-6 mx-auto">	
+                        <a href="inscription.php" class="btn btn-primary mb-3">Inscription</a></div>
+            </form>
+    </div>
+</div>       
+</div>
+</div>
+
+ 
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,180 @@
+<?php
+include_once("dbconnect.php");
+
+        @$login = htmlspecialchars($_POST['login']);
+        @$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        if ($_POST == NULL) // génération d'un formulaire de base
+    { 
+        ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Page d'inscription</title>
+</head>
+
+    <header>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Reservation de salles</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Retour</a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="connexion.php">Connexion</a>
+              </li>
+              
+             </ul>
+            </div>
+        </div>
+      </nav>
+</header>
+<body>
+    <div class="parallax_inscription">  
+    <h1 class="text-center" style="padding-top:10%;">Inscription </h1>   
+            
+            
+            
+            <div class="container " id="page_centrale_connexion">
+                <div class="row h-100  ">
+                    <div class="col-12 h-100 d-flex justify-content-center align-items-center">
+                        <form class="w-50" action="inscription.php" method="post">
+                            <div class="form-group form-control-lg">
+                                <label for="login">Choisissez votre Login:</label>
+                                <input type="login" name="login" class="form-control form-control-lg" style="border:1px solid black;" id="login">
+                            </div>
+                            <div class="form-group form-control-lg">
+                                <label for="password">Password:</label>
+                                <input type="password" name="password" class="form-control form-control-lg" style="border:1px solid black;" id="password">
+                            </div>
+                            <div class="form-group form-control-lg">
+                                <label for="confirm_password">Confirmez le Password:</label>
+                                <input type="password" name="confirm_password" class="form-control form-control-lg" style="border:1px solid black;" id="confirm_password">
+                            </div>
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                            <button type="submit" class="btn btn-primary mt-5">S'enregistrer</button>
+    </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+
+            <?php
+        } else {
+            $req = $bdd->prepare(' SELECT * FROM utilisateurs WHERE login = :login '); //on va chercher dans la bdd si le login existe déjà
+            $req->execute(array('login' => $_POST['login']));
+            $donnees = $req->fetch();
+            if (@$donnees['login'] == $_POST['login']) // on compare le résultat, si c'est le cas on générère un form avec le message " login déjà utilisé " 
+            {
+            ?>
+                <div class="container " id="page_centrale_connexion">
+                    <div class="row h-100  ">
+                        <div class="col-12 h-100 d-flex justify-content-center align-items-center">
+                            <form class="w-50" action="inscription.php" method="post">
+                                <div class="form-group form-control-lg">
+                                    <label for="login">Choisissez votre Login:</label>
+                                    <input type="login" name="login" class="form-control form-control-lg" id="login" value="<?php if (isset($login)) {echo $login;} ?>">
+                                </div>
+                                <div class="form-group form-control-lg">
+                                    <label for="password">Password:</label>
+                                    <input type="password" name="password" class="form-control form-control-lg" id="password">
+                                </div>
+                                <div class="form-group form-control-lg">
+                                    <label for="confirm_password">Confirmer le Password:</label>
+                                    <input type="password" name="confirm_password" class="form-control form-control-lg" id="confirm_password">
+                                </div>
+                                <p class="alert alert-danger alert-dismissible fade show">Login déjà utilisé, veuillez en choisir un autre.</p>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            } else {
+                if ($_POST['login'] != NULL and  $_POST['password'] != NULL and  $_POST['confirm_password'] != NULL)
+                // si tous les champs sont remplis, on peu passer à la suite
+                {
+                    if (@$_POST['confirm_password'] === @$_POST['password'])
+                    // on verifie d'abord que les mdp sont bien identiques
+                    {
+                        $req = $bdd->prepare('INSERT INTO utilisateurs(login, password) VALUES(:login, :password)');
+                        $req->execute(array(
+                            'login' => $login,
+                            'password' => $password,));
+                        header('Location: connexion.php'); //redirection
+                    } else
+                    // si mdp non identiques, on génère le formulaire avec un message
+                    { ?>
+                        <div class="container " id="page_centrale_connexion">
+                            <div class="row h-100  ">
+                                <div class="col-12 h-100 d-flex justify-content-center align-items-center">
+                                    <form class="w-50" action="inscription.php" method="post">
+                                        <div class="form-group form-control-lg">
+                                            <label for="login">Choisissez votre Login:</label>
+                                            <input type="login" name="login" class="form-control form-control-lg" id="login" value="<?php if (isset($login)) {echo $login;} ?>">
+                                        </div>
+                                        <div class="form-group form-control-lg">
+                                            <label for="password">Password:</label>
+                                            <input type="password" name="password" class="form-control form-control-lg" id="password">
+                                        </div>
+                                        <div class="form-group form-control-lg">
+                                            <label for="confirm_password">Confirmer le Password:</label>
+                                            <input type="password" name="confirm_password" class="form-control form-control-lg" id="confirm_password">
+                                        </div>
+                                        <p class="alert alert-danger alert-dismissible fade show"><strong>Erreur!</strong>Les mots de passe ne sont pas identiques</p>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                } else
+                // if empty :
+                { ?>
+                    <div class="container " id="page_centrale_connexion">
+                        <div class="row h-100  ">
+                            <div class="col-12 h-100 d-flex justify-content-center align-items-center">
+                                <form class="w-50" action="inscription.php" method="post">
+                                    <div class="form-group form-control-lg">
+                                        <label for="login">Choisissez votre Login:</label>
+                                        <input type="login" name="login" class="form-control form-control-lg" id="login" value="<?php if (isset($login)) echo $login;} ?>">
+                                    </div>
+                                    <div class="form-group form-control-lg">
+                                        <label for="password">Password:</label>
+                                        <input type="password" name="password" class="form-control form-control-lg" id="password">
+                                    </div>
+                                    <div class="form-group form-control-lg">
+                                        <label for="confirm_password">Confirmer le Password:</label>
+                                        <input type="password" name="confirm_password" class="form-control form-control-lg" id="confirm_password">
+                                    </div>
+                                    <p class="alert alert-info alert-dismissible fade show">veuillez remplir tous les champs:</p>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+        <?php
+                }
+            }
+        $bdd = null;
+        ?>
+
+
+    
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</body>
+</html>
