2b9285fb1a73e86ab1458f669bf51dfb09aec3b7
richard-schulze <121094145+richard-schulze@users.noreply.github.com>
2023-02-11 09:07:54 +0100

    Update planning.php

--- planning.php
+++ planning.php
@@ -4,9 +4,13 @@ session_start();
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Planning</title>
-  <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <title>Planning</title>
+      
 </head>
 <body>
 
@@ -80,15 +84,19 @@ session_start();
 				?>
 					<form method="get" action="reservation.php">	
 						<!-- <input name="id" class="btn btn-info" type="submit" value="Detail" > -->
-						<button class="boutondetail" type="submit" name="id" value="<?=$value[0]?>" style="border-radius:20px;background-color:cornflowerblue;padding:0 10px 0 10px;">Detail</button>
+					<?php
 
+    					if (isset($_SESSION['login']) === TRUE) { ?>
+						<button class="boutondetail" type="submit" name="id" value="<?=$value[0]?>" style="border-radius:20px;background-color:cornflowerblue;padding:0 10px 0 10px;">Detail</button>
 						<input name="test" type="hidden" value="<?php echo $value[0];
 						// var_dump($value);
 						 ?>">  
-					
-					</form>	
-				<?php					
-				}
+						</form>	
+						<?php }else{?>
+							
+							<p style="font-size:10px;">(Vous devez vous connecter pour voir le détail)</p>
+						<?php }
+						}
 			}
 				echo "</td>";
 			}
@@ -102,9 +110,13 @@ session_start();
 
 </article>
 
+
+
 </section>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </body>
 
 </html> 
